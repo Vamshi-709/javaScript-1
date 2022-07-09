7fb6f09b2908d7826983c4620b87bbeb81a4b21f
Vamshi <gvamshi906@gmail.com>
2022-07-09 11:48:39 +0530

    callback

--- callback.js
+++ callback.js
@@ -3,11 +3,26 @@ function greet(name, callback) {
     console.log('Hi' + ' ' + name);
     callback();
 }
-
-// callback function
 function callMe() {
     console.log('I am callback function');
 }
-
-// passing function as an argument
 greet('Vamshi', callMe);
+
+// numbers
+function filter(numbers, callback) {
+    let results = [];
+    for (const number of numbers) {
+      if (callback(number)) {
+        results.push(number);
+      }
+    }
+    return results;
+  }
+  
+  let numbers = [1, 2, 4, 7, 3, 5, 6];
+  
+  let oddNumbers = filter(numbers, function (number) {
+    return number % 2 != 0;
+  });
+  
+  console.log(oddNumbers);
