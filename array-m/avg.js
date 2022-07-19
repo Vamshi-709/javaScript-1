1dd712efbf916713c29fbc63f70ba4f44e411989
Vamshi <gvamshi906@gmail.com>
2022-07-19 16:46:30 +0530

    logic

---
+++ array-m/avg.js
@@ -0,0 +1,4 @@
+const array = [1,3,2,5,6,7]
+const avarage = array.reduce((a,b)=> a+b ,0) /array.length
+
+console.log(avarage)
---
+++ array-m/total.js
@@ -0,0 +1,3 @@
+const array =[ 1,2,3,4,5,6]
+const total = array.reduce((a,b)=> a +b ,0)
+console.log(total);
